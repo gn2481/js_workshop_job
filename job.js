2e8151af3d80f5aa8a07d24ecc1f3499c967c9f9
gn2481 <gn2481@gmail.com>
2020-08-25 16:33:16 +0800

    task_3_done

--- job.js
+++ job.js
@@ -1,11 +1,20 @@
 const form = document.forms[0]
-form.addEventListener('submit', (e) => {
+const template = document.querySelector('template')
+const temJob = template.content.querySelector('.job') 
+const temCompany = template.content.querySelector('.company')
+const temFullTime = template.content.querySelector('.fulltime')
+const temLocation = template.content.querySelector('.location')
+const jobPannel = document.getElementById('job-pannel')
+form.addEventListener('submit', () => {
+  event.preventDefault()
+  jobPannel.innerHTML = " "
+
   let desc = form.description.value
   let location = form.location.value
+ 
   if(form.full_time.checked){
     var fullTime = "on"
   }
-  e.preventDefault()
   // fetch 寫法
   // fetch('https://still-spire-37210.herokuapp.com/positions.json')
   //     .then(request => {console.log(request.json())})
@@ -21,8 +30,20 @@ form.addEventListener('submit', (e) => {
     }
   })
   .then(function (response) {
+
     let data = response.data
     console.log(data)
+    data.forEach(obj => {
+      let link = data.url
+      temJob.setAttribute('href', `${link}`)
+      temJob.textContent = obj.title
+      temCompany.textContent = obj.company
+      temFullTime.textContent = obj.type
+      temLocation.textContent = obj.location
+      const clone = document.importNode(template.content, true);
+      jobPannel.prepend(clone)
+    })
+    
   })
   .catch(function (error) {
     console.log(error);
